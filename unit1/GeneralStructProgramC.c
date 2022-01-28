3f4bc99590e48b2fabf55d3792df47d20f5e767e
MarianoMS56 <Marianomirandasanchez56@gmail.com>
2022-01-28 08:40:24 -0600

    Estructura general C

---
+++ unit1/GeneralStructProgramC.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global Variables *//;
+//Create a Variable: datatype nameVariable = anyData;
+//datatypes in C/C++/C#/PHP 
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "Mariano Miranda";
+char unCaracter = 'M';
+
+
+
+
+int main(){
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
