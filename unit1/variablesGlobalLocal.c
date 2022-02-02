092c5bed13bf2da86f841103d8212b6cafb0bcf6
MarianoMS56 <Marianomirandasanchez56@gmail.com>
2022-02-02 11:08:43 -0600

    local variables internal main

---
+++ unit1/variablesGlobalLocal.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//*Global Variables *//;
+//Create a Variable: datatype nameVariable = anyData;
+//datatypes in C/C++/C#/PHP 
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "Mariano Miranda";
+char unCaracter = 'M';
+int temperatura= 256;
+
+
+/////la ejecucion del programa/////
+int main(){
+    temperatura = 24;
+    int hours = 12;
+    printf ("hours: %i\n", hours);
+    hours = hours + 2;
+    printf("hours: %i\n", hours);
+    printf("Hello Data B %s\n", name);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
