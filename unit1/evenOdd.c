ce59a97b010239f4f08b638d1c1fee446679fda4
MarianoMS56 <Marianomirandasanchez56@gmail.com>
2022-02-19 20:56:36 -0600

    Added new files

--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -1,15 +1,14 @@
-#include <stdio>
-#include <stdcool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-
-
-int main(){
-    int numberToTest = 0;
-    int result = numberToTest % 2;
-    bool hasModule = (result > 0);
-
-    if(hasModule){
-
-} else{
-
+int main(int argc, char **argv) {
+    const char *const even_or_odd[] = { "even", "odd" };
+    for (int i = 1; i < argc; i ++) {
+        const int n = atoi(argv[i]);
+        printf("%d is %s\n",
+               n,
+               even_or_odd[(int)fmod((unsigned)n, 2.0)]);
+    }
+    return 0;
 }
